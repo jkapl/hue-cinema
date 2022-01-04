83ff2941ed0aa3d2ce6d926d7b9a3a392091efcc
Joel <joelkaplan1@gmail.com>
2022-01-03 20:47:25 -0500

    Add initial camera code

--- camera.py
+++ camera.py
@@ -1,2 +1,14 @@
 import cv2
+from picamera.array import PiRGBArray
 from picamera import PiCamera
+import numpy as np
+
+cv2.namedWindow("Trackbars")
+ 
+cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
